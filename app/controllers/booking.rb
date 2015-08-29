6749ec1be51b48089000426c747c0c78fe48c8e3
Chester John <chesterjohn@Chesters-MBP.lan>
2015-08-30 03:30:19 +0800

    bookings booleans fixed

--- app/controllers/booking.rb
+++ app/controllers/booking.rb
@@ -1,12 +1,15 @@
 post '/listings/:id/booking' do
+
 	@booking = params[:booking]
 	@user = User.find(current_user.id)
 	@listing = Listing.find(params[:id])
 	@all_bookings = Booking.all
 	if @all_bookings.find_by(start_date: @booking[:start_date]).nil?
 		@new_booking = @user.bookings.create(start_date: @booking[:start_date], end_date: @booking[:end_date])
+		byebug
 		@listing.bookings << @new_booking
-		@listing[:booked] = true
+		@listing.booked = true
+		@listing.save
 		redirect '/thank_you'
 	else
 		redirect "/listings/#{@listing.id}/book"
