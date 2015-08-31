527ce7efa605c8a33a4f05bbca278973f747e59f
Chester John <chesterjohn@Chesters-MBP.lan>
2015-08-31 16:23:30 +0800

    add js to delete button

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $(".delete").click(function() {
+    if(confirm("Sure you want to delete?")){
+    	$(this).closest('tr').delay().fadeOut();
+    }
+    else {
+    	return false;
+    }
+  });
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
+
+
